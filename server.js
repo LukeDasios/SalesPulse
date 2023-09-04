240ffc1cdb8311b39210d6edff8e3bf898f8826a
Luke Dasios <lukedasios@321@gmail.com>
2023-09-04 14:30:34 -0400

    finished prototype time to start actual project

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import bodyParser from "body-parser";
-import icecreamFlavoursRoutes from "./routes/icecreamflavours.js";
-
-const app = express();
-app.use(bodyParser.json());
-app.use("/icecreamflavours", icecreamFlavoursRoutes);
-
-app.listen(3000, () => console.log("Server running on localhost:3000"));
